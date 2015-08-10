7c6bcdd303f3e2452565d97705d9280b602dca4c
Stephen O'Sullivan <osullisg@gmail.com>
2015-08-09 18:33:33 -0700

    Finished item31

--- Chapter4.py
+++ Chapter4.py
@@ -2,6 +2,7 @@ __author__ = 'stephenosullivan'
 
 
 from datetime import *
+import weakref
 
 class Item29:
     """
@@ -143,7 +144,7 @@ class BucketV2:
         self.quota_consumed = 0
 
     def __repr__(self):
-        return ('Bucket(max_quota=%d, quota_consumed=%d)' % (self.max_quota, self.quota_consumed))
+        return 'Bucket(max_quota=%d, quota_consumed=%d)' % (self.max_quota, self.quota_consumed)
 
     @property
     def quota(self):
@@ -162,6 +163,105 @@ class BucketV2:
             assert self.max_quota >= self.quota_consumed
             self.quota_consumed += delta
 
+
+class Item31:
+    """
+    Use descriptors for reusable @property methods
+    """
+    def __init__(self):
+        galileo = Homework()
+        galileo.grade = 95
+
+        exam = Exam()
+        exam.writing_grade = 40
+
+        first_exam = Examv2()
+        first_exam.writing_grade = 82
+        second_exam = Examv2()
+        second_exam.writing_grade = 75
+        print('First ', first_exam.writing_grade, 'is right')
+        print('First ', second_exam.writing_grade, 'is right')
+
+
+
+
+# Validate that the grade received is a percentage
+class Homework(object):
+    def __init__(self):
+        self._grade = 0
+
+    @property
+    def grade(self):
+        return self._grade
+
+    @grade.setter
+    def grade(self, value):
+        if not (0 <= value <= 100):
+            raise ValueError('Grade must be between zero and 100')
+        self._grade = value
+
+#Grade for exam
+class Exam(object):
+    def __init__(self):
+        # multiple parts of grade
+        self._writing_grade = 0
+        self._math_grade = 0
+
+    @staticmethod
+    def _check_grade(value):
+        if not (0 <= value <= 100):
+            raise ValueError('Grade must be between 0 and 100')
+
+    @property
+    def writing_grade(self):
+        return self._writing_grade
+
+    @writing_grade.setter
+    def writing_grade(self, value):
+        self._check_grade(value)
+        self._writing_grade = value
+
+    @property
+    def math_grade(self):
+        return self._math_grade
+
+    @math_grade.setter
+    def math_grade(self, value):
+        self._check_grade(value)
+        self._math_grade = value
+
+
+# Use descriptors instead
+# Save each instance in a dict
+class Grade:
+    def __init__(self):
+        self._values = {}
+        #self._values = weakref.WeakKeyDictionary()
+
+    def __get__(self, instance, instance_type):
+        if instance is None:
+            return self
+        return self._values.get(instance, 0)
+
+    def __set__(self, instance, value):
+        if not (0 <= value <= 100):
+            raise ValueError('Grade must be between 0 and 100')
+        self._values[instance] = value
+
+
+
+class Examv2:
+    math_grade = Grade()
+    writing_grade = Grade()
+    science_grade = Grade()
+
+
+
+
+
+
+
 if __name__ == '__main__':
     sol = Item29()
     sol = Item30()
+    sol = Item31()
